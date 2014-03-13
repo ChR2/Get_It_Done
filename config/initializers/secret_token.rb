833a9501b73c3bd5355b37e2d2a1771e483abc8d
Wendy Sanchez-Vaynshteyn <WendyGSV@gmail.com>
2014-03-13 12:45:25 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GetItDone::Application.config.secret_key_base = 'c09ac0f281a3e3b805cd953ca511d2a08e17c2f8c06a7c9c0386bbcdcce1a9edca68f797a859472a373d39702dbf670edd7f12a9bd1f920cddca66a17c275d7d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GetItDone::Application.config.session_store :cookie_store, key: '_Get_It_Done_session'
